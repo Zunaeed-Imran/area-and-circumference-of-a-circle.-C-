eb2a6817f577cbe3529becc1a4ff12f573d1c284
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-29 19:40:40 +0600

    area of circle in javaScript

---
+++ circle_area.js
@@ -0,0 +1,8 @@
+let circle = prompt("Enter the radius of circle: ");
+
+let area = Math.PI * Math.pow(circle, 2);
+
+document.write("The area of circle is: " + area);
+
+
+// A = \pi r^2  (the formula)
